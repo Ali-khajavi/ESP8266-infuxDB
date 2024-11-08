8956b733e62beda7010b08159788a957e9d3168c
Ali <54626264+Ali-khajavi@users.noreply.github.com>
2024-11-08 13:36:57 +0100

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,7 @@
 #include <InfluxDbClient.h>
 #include <InfluxDbCloud.h>
 
-
-// Define Wifi Connections
+// Define WiFi Connections
 #define WIFI_SSID "Mr.Green"
 #define WIFI_PASSWORD "alialiali"
 #define WIFI_SSID1 "Pillipp-Mobil"
@@ -13,128 +12,91 @@
 #define WIFI_SSID0 "Monitoring"
 #define WIFI_PASSWORD0 "Haustechnik24"
 
-#define INFLUXDB_URL "http://172.104.132.172:8086"
-#define INFLUXDB_TOKEN "as-RsgomyvthozUA1YzzUji5uwjRBcRZgB5xLkYmQmTTZ8mdSDiziVfzxqjTUqKPND27P1jCrnwvjGTm6CsXlg=="
-#define INFLUXDB_ORG "9658415ef121eada"
+#define INFLUXDB_URL "http://172.105.78.144:8086"
+#define INFLUXDB_TOKEN "QDp-QDPqKbNbVPjESvLcj7ons1LzjD9AUiEkYs_s5Nc8yRtTiiysH8eITv4hHqC87o7W7pT2_VW-B28MLb-QXQ=="
+#define INFLUXDB_ORG "ec44f19893e990b6"
 #define INFLUXDB_BUCKET "Sensors"
-
-#define DEVICE "ESP8266" 
-
-//#define TZ_INFO "WET0WEST,M3.5.0/1,M10.5.0"
+#define DEVICE "2024" 
 
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
-
-Point sensor("Flow_Rate");
-
+Point sensor("Jürgen Pillipp");
 ESP8266WiFiMulti wifi_multi;
 
-uint16_t connectTimeOutPerAP=5000;
-
-
-
+const uint16_t CONNECT_TIMEOUT = 5000;
 volatile unsigned int pulseCount = 0;
 
+const float CONVERSION_FACTOR = 0.45;
+const float ACCURACY_FACTOR = 0.03;
 
-float flow_Rate = 0;
-const float conversionFactor = 0.45;
-const float accuracyFactor = 0.03;
-
-
-bool start_flag = 0;
-
-
-long currentMillis = 0;
-long previousMillis = 0;
-int interval = 1000;
-float calibrationFactor = 0.45;
-byte pulse1Sec = 0;
-float flowRate;
-unsigned long flowMilliLitres;
-unsigned int totalMilliLitres;
-float flowLitres;
-float totalLitres;
-
-
-float calculationFlow(unsigned long frequency){
-
-  float maxFlow = (frequency * (1 + accuracyFactor)) / conversionFactor;
-  float minFlow = (frequency * (1 - accuracyFactor)) / conversionFactor;
-  Serial.println("");
-  Serial.print("This is the pulse : ");
-  Serial.println(frequency);
-  return (maxFlow + minFlow) / 2.0;
+void IRAM_ATTR pulseCounter() {
+    pulseCount++;
 }
 
-void IRAM_ATTR  pulseCounter(){
-  pulseCount++;
+float calculateFlowRate(unsigned long frequency) {
+    float maxFlow = (frequency * (1 + ACCURACY_FACTOR)) / CONVERSION_FACTOR;
+    float minFlow = (frequency * (1 - ACCURACY_FACTOR)) / CONVERSION_FACTOR;
+    return (maxFlow + minFlow) / 2.0;
 }
 
 void setup() {
-  // power sensor on
-  pinMode(D6, OUTPUT);
-  digitalWrite(D6, HIGH);
+    Serial.begin(115200);
+    pinMode(10, OUTPUT); 
+    digitalWrite(10, HIGH); // Power Up The Sensor
 
-  // read sensor data
-  pinMode(D5, INPUT);
-  attachInterrupt(digitalPinToInterrupt(D5), pulseCounter , RISING);
-  delay(80);
+    pinMode(D1, INPUT);  // Read sensor channel 1 data
+    attachInterrupt(digitalPinToInterrupt(D1), pulseCounter, RISING); // Set intrupt Registery active to be able to count digital puls.
 
-  // calculate pulse in 1 second
-  previousMillis = millis();
-  while(currentMillis <  previousMillis + 3000 ){
-    currentMillis = millis();
-    pulse1Sec = pulseCount;
-  }
+    pulseCount = 0;  // Reset pulse count
+    unsigned long startMillis = millis();
 
-  Serial.begin(115200);
+    // Count pulses for exactly 2 seconds
+    while (millis() - startMillis < 2000) {
+        // Waiting 2 seconds while counting pulses in the interrupt
+    }
 
-  //flow_Rate = calculationFlow(pulseCount);
-  
-  wifi_multi.addAP(WIFI_SSID0,WIFI_PASSWORD0);
-  wifi_multi.addAP(WIFI_SSID,WIFI_PASSWORD);
-  wifi_multi.addAP(WIFI_SSID1,WIFI_PASSWORD1);
-  
-  
-  Serial.println("");
-  Serial.print("number of pulse is :");
-  Serial.println(pulseCount);
-  Serial.println("Connecting to WiFi....");
+    // Calculate frequency from pulse count over 2-second interval
+    float frequency = pulseCount / 2.0;
+    float flowRate = calculateFlowRate(frequency);
 
-  int i = 0;
-  while(wifi_multi.run(connectTimeOutPerAP)!=WL_CONNECTED)
-  {
-    delay(500);
-    Serial.println(i);
-    i++;
-  }
+    Serial.println("Connecting to WiFi...");
+    wifi_multi.addAP(WIFI_SSID0, WIFI_PASSWORD0);
+    wifi_multi.addAP(WIFI_SSID, WIFI_PASSWORD);
+    wifi_multi.addAP(WIFI_SSID1, WIFI_PASSWORD1);
 
-  delay(1000);
+    while (wifi_multi.run(CONNECT_TIMEOUT) != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
 
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    Serial.print("Wifi Connected to :");
-    Serial.println(WiFi.SSID());
-  }else{
-    Serial.println("Wifi connection problem!!!!!!!!");
-  }
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.print("Connected to WiFi: ");
+        Serial.println(WiFi.SSID());
 
-  flowRate = ((1000.0 / (currentMillis - previousMillis)) * pulse1Sec) / calibrationFactor;
+        // Send flow rate to InfluxDB
+        sensor.addTag("DEVICE", DEVICE);
+        sensor.addField("flow", flowRate);
+        client.writePoint(sensor);
+        
+        Serial.print("Flow rate: ");
+        Serial.println(flowRate);
+        if (client.writePoint(sensor)) {
+          Serial.println("Data successfully sent to InfluxDB");
+        }
+        else {
+        Serial.print("InfluxDB write failed: ");
+        Serial.println(client.getLastErrorMessage());
+        }
+      } 
+      else {
+        Serial.println("WiFi connection failed!");
+      }
 
-
-  Serial.print("flow : ");
-  Serial.println(flowRate);
-
-  sensor.addTag("device", DEVICE);
-  //timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
-  sensor.addField("flow", flowRate);
-  client.writePoint(sensor);
-  sensor.clearFields();
-  digitalWrite(D6, LOW);
-  WiFi.mode(WIFI_OFF);
-  Serial.println("Good Night for 30 Seconds");
-  ESP.deepSleep(30e6);
+    // Power down sensor, disable WiFi, and put ESP to sleep
+    digitalWrite(10, LOW);
+    WiFi.mode(WIFI_OFF);
+    ESP.deepSleep(30e6);
 }
 
 void loop() {
-  
+    // Empty loop since ESP will deep sleep after setup
 }
